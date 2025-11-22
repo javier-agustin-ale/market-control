de7bb6d163de6ced60038d3dbe2ec7b8e03e60c9
JavierAle <javieragustinale@gmail.com>
2025-11-22 10:01:17 +0100

    Working on the checkout component and the product-card that will be re used

--- frontend/haiilomarkt-fe/src/app/components/product-card/product-card.component.ts/product-card.component.ts
+++ frontend/haiilomarkt-fe/src/app/components/product-card/product-card.component.ts/product-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
   standalone: true,
