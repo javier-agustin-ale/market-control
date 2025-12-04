d60c69e10993fa5dbd0f3af902dc9e00e76aa4f8
JavierAle <javieragustinale@gmail.com>
2025-12-04 14:15:41 +0100

    Added test for addNewProduct service product

--- frontend/haiilomarkt-fe/src/app/services/product-service/product.service.spec.ts
+++ frontend/haiilomarkt-fe/src/app/services/product-service/product.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { provideZonelessChangeDetection } from '@angular/core';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +14,31 @@ describe('ProductService', () => {
       providers: [provideZonelessChangeDetection()],
     });
     service = TestBed.inject(ProductService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add new product', () => {
+    const newProductData = new FormData();
+    newProductData.append('Name', 'Apple');
+
+    const mockProduct: any = {
+      id: 1,
+      name: 'Apple',
+      price: 1000,
+    };
+
+    service.addNewProduct(newProductData).subscribe((product) => {
+      expect(product).toEqual(mockProduct);
+    });
+
+    const req = httpMock.expectOne(service['apiUrl']);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(newProductData);
+
+    req.flush(mockProduct);
   });
 });
