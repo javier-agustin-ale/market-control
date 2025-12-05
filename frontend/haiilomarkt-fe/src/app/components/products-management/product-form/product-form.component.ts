f783d80b4b5769280a981659a800d57c59977c16
JavierAle <javieragustinale@gmail.com>
2025-12-05 12:15:20 +0100

    Removed ngOnChanges and replaced it with input setter/getter

--- frontend/haiilomarkt-fe/src/app/components/products-management/product-form/product-form.component.ts
+++ frontend/haiilomarkt-fe/src/app/components/products-management/product-form/product-form.component.ts
@@ -1,12 +1,5 @@
-import {
-  Component,
-  inject,
-  Input,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+// filepath: /Users/javier/Desktop/haiilomarkt/frontend/haiilomarkt-fe/src/app/components/products-management/product-form/product-form.component.ts
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -39,10 +32,18 @@ import { ProductService } from '../../../services/product-service/product.servic
   styleUrl: './product-form.component.scss',
   standalone: true,
 })
-export class ProductFormComponent implements OnInit, OnChanges, OnDestroy {
-  @Input() productToEdit: Product | null = null;
+export class ProductFormComponent implements OnInit, OnDestroy {
+  private _productToEdit: Product | null = null;
+  @Input() set productToEdit(value: Product | null) {
+    this._productToEdit = value;
+    if (this.formProduct) this.fillFormWithEditProduct(value);
+  }
+  get productToEdit(): Product | null {
+    return this._productToEdit;
+  }
+
   public formProduct!: FormGroup;
-  public containsOffer: boolean = false;
+  public containsOffer = false;
 
   private file: File | null = null;
   private containsOfferSubscription!: Subscription;
@@ -53,19 +54,7 @@ export class ProductFormComponent implements OnInit, OnChanges, OnDestroy {
 
   public ngOnInit(): void {
     this.setUpForm();
-  }
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes['productToEdit'] && changes['productToEdit'].currentValue) {
-      this.formProduct.patchValue(changes['productToEdit'].currentValue);
-
-      this.addExistingImageToFile();
-      if (this.productToEdit?.offerAmount) {
-        this.formProduct.get('containsOffer')?.setValue(true);
-        return;
-      }
-      this.formProduct.get('containsOffer')?.setValue(false);
-    }
+    this.fillFormWithEditProduct(this._productToEdit);
   }
 
   public onFileSelected(event: Event): void {
@@ -75,15 +64,19 @@ export class ProductFormComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  public clearForm(): void {
-    this.productManagmentService.selectedProductToEdit(null);
-    this.formProduct.reset({
-      name: '',
-      unitPrice: null,
-      offerAmount: null,
-      offerPrice: null,
-      containsOffer: false,
-    });
+  public clearForm(notifyService: boolean = true): void {
+    if (notifyService) this.productManagmentService.selectedProductToEdit(null);
+
+    this.formProduct.reset(
+      {
+        name: '',
+        unitPrice: null,
+        offerAmount: null,
+        offerPrice: null,
+        containsOffer: false,
+      },
+      { emitEvent: false }
+    );
     this.file = null;
     this.formProduct.markAsPristine();
     this.formProduct.markAsUntouched();
@@ -105,7 +98,7 @@ export class ProductFormComponent implements OnInit, OnChanges, OnDestroy {
       this.formProduct.get('offerAmount')?.setValue(null);
       this.formProduct.get('offerPrice')?.setValue(null);
     }
-    const { containsOffer, ...payload } = this.formProduct.value;
+    const { containsOffer, ...payload } = this.formProduct.value as any;
 
     const productData = new FormData();
     Object.entries(payload).forEach(([key, value]) => {
@@ -125,19 +118,29 @@ export class ProductFormComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  private addExistingImageToFile(): void {
-    if (!this.productToEdit) return;
-    const byteArray = new Uint8Array(this.productToEdit.image.data);
+  private addExistingImageToFile(product: Product | null): void {
+    if (!product || !(product as any).image?.data) return;
+    const byteArray = new Uint8Array((product as any).image.data);
     const blob = new Blob([byteArray], { type: 'image/png' });
 
-    const fileFromExistingImage = new File(
-      [blob],
-      this.productToEdit.name.replace(/\s/g, '') + '.png',
-      {
-        type: 'image/png',
-      }
-    );
+    const fileFromExistingImage = new File([blob], product.name.replace(/\s/g, '') + '.png', {
+      type: 'image/png',
+    });
     this.file = fileFromExistingImage;
+  }
+
+  private fillFormWithEditProduct(product: Product | null): void {
+    if (!this.formProduct) return;
+
+    if (!product) {
+      this.clearForm(false);
+      return;
+    }
+    this.formProduct.patchValue(product as any, { emitEvent: false });
+
+    this.addExistingImageToFile(product);
+    const hasOffer = !!(product as any).offerAmount;
+    this.formProduct.get('containsOffer')?.setValue(hasOffer);
   }
 
   private setUpForm(): void {
