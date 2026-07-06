938966a6789e3486666242a405018c03e7c962f2
javier-agustin-ale <javieragustinale@gmail.com>
2026-07-06 16:01:31 +0200

    Fixed building and some unit test

--- frontend/market-control/proxy.conf.js
+++ frontend/market-control/proxy.conf.js
@@ -1,10 +1,10 @@
 // proxy.conf.js
 export default [
   {
-    context: ['/api'], // <--- ¡IMPORTANTE! Solo "/api", NUNCA pongas "/"
+    context: ['/api'],
     target: 'http://localhost:3000',
     secure: false,
     changeOrigin: true,
-    logLevel: 'debug', // Esto nos ayudará a ver en la terminal qué pasa
+    logLevel: 'debug',
   },
 ];
--- frontend/market-control/src/app/features/auth/log-in-form/log-in-form.component.spec.ts
+++ frontend/market-control/src/app/features/auth/log-in-form/log-in-form.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { LogInFormComponent } from './log-in-form.component';
 
 describe('LogInFormComponent', () => {
@@ -8,7 +11,8 @@ describe('LogInFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LogInFormComponent],
+      imports: [LogInFormComponent, HttpClientTestingModule],
+      providers: [provideZonelessChangeDetection(), { provide: MatDialogRef, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LogInFormComponent);
