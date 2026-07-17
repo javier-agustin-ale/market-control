85b676cc283e8a7f830e626b60d79b7478decc5f
javier-agustin-ale <javieragustinale@gmail.com>
2026-07-17 17:03:03 +0200

    Small change in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,23 @@ import authRoutes from "./routes/authRoutes.js";
 import marketControlRoutes from "./routes/marketControlRoutes.js";
 import { seedDefaultAdmin } from "./seeders/defaultAdminSeeder.js";
 
-import "./models/user.js";
 import "./models/product.js";
+import "./models/user.js";
 
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/", marketControlRoutes);
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || process.env.APP_PORT || 3000;
 
 sequelize
   .sync({ alter: true })
