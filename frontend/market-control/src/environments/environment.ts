773192234e74ce3900225eb98ba53515c22b90cd
javier-agustin-ale <javieragustinale@gmail.com>
2026-08-02 12:09:53 +0200

    Changeed prod api

--- frontend/market-control/src/environments/environment.ts
+++ frontend/market-control/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://market-control-k7z5n1ju9-javier-agustin-ales-projects.vercel.app/api'
+  apiUrl: 'https://market-control-five.vercel.app/api',
 };
